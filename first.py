086363b8b3255a57adbf2a7c811d8dbaa70d98e6
Yanik <user@MacBook-Pro.local>
2024-01-26 00:41:12 +0300

    here we go. Create some kind of element and in future I will made, with help of course, a main loop

--- first.py
+++ first.py
@@ -1,1 +1,19 @@
-print('hi')
+import pygame as pg  # call the pygame
+
+pg.init()
+
+BLACK = (0, 0, 0)
+GREY = (128, 128, 128)
+YELLOW = (255, 255, 0)
+
+WIDTH, HEIGHT = 800, 800
+TILE_SIDE = 20
+GRID_WIDTH = WIDTH // TILE_SIDE
+GRID_GEIGHT = HEIGHT // TILE_SIDE
+FPS = 60
+
+screen = pg.display.set_mode((WIDTH, HEIGHT))  # initialize new pygame window
+
+clock = pg.time.Clock()
+
+# writing main loop (what?)
